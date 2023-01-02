163661cdf12982d4600a6b7714e2bfb0f8df2834
reemaidris1 <reema.idris0@gmail.com>
2023-01-01 21:07:14 -0500

    simple basic project

---
+++ src/class03/class01.java
@@ -0,0 +1,12 @@
+package class03;
+
+public class class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hey this is a git class");
+		
+	}
+
+}
